0d27893a881bca377e82b51b15c66e0ca19c0ea3
rafaelleitedasilva <rafael.leite.14@hotmail.com>
2023-09-18 07:24:25 -0300

    Style Prettier: Configurando Prettier para estilização dos arquivos

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## BlogText
 
 ![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=BLUE&style=for-the-badge)
+
 <p align="left">
 <a href="https://github.com/laravel/framework/actions"><img src="https://github.com/laravel/framework/workflows/tests/badge.svg" alt="Build Status"></a>
 <!-- <a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,20 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
-    server: {
-        https: false,
-        host: true,
-        strictPort: true,
-        port: 3009,
-        hmr: {host: 'localhost', protocol: 'ws'},
-        watch: {
-            usePolling: true,
-        }
-    },
+     plugins: [
+          laravel({
+               input: ['resources/css/app.css', 'resources/js/app.js'],
+               refresh: true,
+          }),
+     ],
+     server: {
+          https: false,
+          host: true,
+          strictPort: true,
+          port: 3009,
+          hmr: { host: 'localhost', protocol: 'ws' },
+          watch: {
+               usePolling: true,
+          },
+     },
 });
