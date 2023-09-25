30b6ecc77e5edf5a0dec5ab40f54ec67161a2418
Rafael Leite <rafael.leite.14@hotmail.com>
2023-09-25 14:21:20 -0300

    Feat ImageEmail: Removing If of Email Markdown

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{Vite::asset('resources/images/logo.jpg')}}" class="logo" alt="BlogText Logo">
-@else
-{{ $slot }}
-@endif
+<img src="{{Vite::asset('resources/images/blogtext-logo.png')}}" class="logo" alt="BlogText Logo">
 </a>
 </td>
 </tr>
