4009d032044252207252ca465455b1c61f5f3792
Rafael Leite <rafael.leite.14@hotmail.com>
2023-10-02 08:08:33 -0300

    Feat Test: Creating testing environment and email test suitcase with two examples

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ArtisanController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\{WelcomeController,HomeController};
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +15,14 @@ use App\Http\Controllers\WelcomeController;
 |
 */
 
-Route::get('/', function () {
-     return view('welcome');
-})->name('welcome');
+Route::get('/', function () {return view('welcome');})
+->name('welcome');
 
-Route::post('email-confirmacao', [WelcomeController::class, 'email'])->name(
-     'email-confirmacao'
-);
+Route::get('home', [HomeController::class, 'view'])
+->name('home.view');
 
-Route::get('confirmaEmail', [WelcomeController::class, 'confirmaEmail'])->name(
-     'confirmaEmail'
-);
+Route::post('email', [WelcomeController::class, 'send'])
+->name('email.send');
+
+Route::get('email', [WelcomeController::class, 'view'])
+->name('email.view');
